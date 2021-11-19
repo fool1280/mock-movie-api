ee44246a748c47c4fb22ffc1fe3fe37fa24d776c
mstacha <47231953+mstacha@users.noreply.github.com>
2021-11-19 10:47:42 -0600

    Fixed schema and movie id for reviews

--- main.py
+++ main.py
@@ -31,14 +31,14 @@ def getGenres():
 
     # Create table genres with primary id and name
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS genres (genre_id int PRIMARY KEY, name varchar);")
+        "CREATE TABLE IF NOT EXISTS Genre (id int PRIMARY KEY, name varchar);")
 
     # Insert data into table
     global genres
     for i in tqdm(range(len(response))):
         genres[response[i]['id']] = response[i]['name']
         cursor.execute(
-            "INSERT INTO genres (genre_id, name) VALUES (%s, %s) ON CONFLICT (genre_id) DO NOTHING;", (response[i]['id'], response[i]['name']))
+            "INSERT INTO Genre (id, name) VALUES (%s, %s) ON CONFLICT (id) DO NOTHING;", (response[i]['id'], response[i]['name']))
 
     # Make the changes to the database persistent
     conn.commit()
@@ -58,7 +58,7 @@ def getMovies():
     cursor = conn.cursor()
 
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS movies (id int PRIMARY KEY, original_language varchar, title varchar, overview varchar, release_date varchar, poster_path varchar, vote_average real, genre_id int REFERENCES genres(genre_id));")
+        "CREATE TABLE IF NOT EXISTS Movie (id int PRIMARY KEY, original_language varchar, title varchar, overview varchar, release_date varchar, poster_path varchar, vote_average real, genre_id int REFERENCES Genre(id));")
     global movies
     for id in genres:
         for i in tqdm(range(1, 2)):
@@ -70,7 +70,7 @@ def getMovies():
                 if movie['id'] not in movies:
                     movies.add(movie['id'])
                 cursor.execute(
-                    "INSERT INTO movies (id, original_language, title, overview, release_date, poster_path, vote_average, genre_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;", (movie['id'], movie['original_language'], movie['title'], movie['overview'], movie['release_date'], movie['poster_path'], movie['vote_average'], id))
+                    "INSERT INTO Movie (id, original_language, title, overview, release_date, poster_path, vote_average, genre_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;", (movie['id'], movie['original_language'], movie['title'], movie['overview'], movie['release_date'], movie['poster_path'], movie['vote_average'], id))
     conn.commit()
     conn.close()
     cursor.close()
@@ -88,7 +88,7 @@ def getReviews():
     cursor = conn.cursor()
 
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS reviews (review_id varchar PRIMARY KEY, author varchar, content varchar, movie_id int REFERENCES movies(id));")
+        "CREATE TABLE IF NOT EXISTS Review (id varchar PRIMARY KEY, author varchar, content varchar, movie_id int REFERENCES Movie(id));")
     for id in movies:
         for count in tqdm(range(1, 4)):
             response = requests.get(
@@ -97,8 +97,8 @@ def getReviews():
             if len(response) > 0:
                 for i in tqdm(range(len(response))):
                     review = response[i]
-                    cursor.execute("INSERT INTO reviews (review_id, author, content, movie_id) VALUES (%s, %s, %s, %s) ON CONFLICT (review_id) DO NOTHING;",
-                                   (review['id'], review['author'], review['content'], 559))
+                    cursor.execute("INSERT INTO Review (id, author, content, movie_id) VALUES (%s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;",
+                                   (review['id'], review['author'], review['content'], id))
     conn.commit()
     conn.close()
     cursor.close()
