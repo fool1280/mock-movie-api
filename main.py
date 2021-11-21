01e334d12628bfb3dc951917d8193497cfd391bd
unknown <mstacha@tamu.edu>
2021-11-21 09:18:23 -0600

    adjusted schema to conform with 310 phase 2 document and API

--- main.py
+++ main.py
@@ -32,14 +32,14 @@ def getGenres():
 
     # Create table genres with primary id and name
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS genres (genre_id int PRIMARY KEY, name varchar);")
+        "CREATE TABLE IF NOT EXISTS Genre (id int PRIMARY KEY, name varchar);")
 
     # Insert data into table
     global genres
     for i in tqdm(range(len(response))):
         genres[response[i]['id']] = response[i]['name']
         cursor.execute(
-            "INSERT INTO genres (genre_id, name) VALUES (%s, %s) ON CONFLICT (genre_id) DO NOTHING;", (response[i]['id'], response[i]['name']))
+            "INSERT INTO Genre (id, name) VALUES (%s, %s) ON CONFLICT (id) DO NOTHING;", (response[i]['id'], response[i]['name']))
 
     # Make the changes to the database persistent
     conn.commit()
@@ -59,7 +59,7 @@ def getMovies():
     cursor = conn.cursor()
 
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS movies (movie_id int PRIMARY KEY, original_language varchar, title varchar, overview varchar, release_date varchar, poster_path varchar, vote_average real, genre_id int REFERENCES genres(genre_id));")
+        "CREATE TABLE IF NOT EXISTS Movie (id int PRIMARY KEY, original_language varchar, title varchar, overview varchar, release_date varchar, poster_path varchar, vote_average real, genre_id int REFERENCES Genre(id));")
     global movies
     for id in genres:
         for i in tqdm(range(1, 6)):
@@ -74,7 +74,7 @@ def getMovies():
                 if movie['id'] not in movies:
                     movies.add(movie['id'])
                 cursor.execute(
-                    "INSERT INTO movies (movie_id, original_language, title, overview, release_date, poster_path, vote_average, genre_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (movie_id) DO NOTHING;", (movie['id'], movie['original_language'], movie['title'], movie['overview'], release_date, movie['poster_path'], movie['vote_average'], id))
+                    "INSERT INTO Movie (id, original_language, title, overview, release_date, poster_path, vote_average, genre_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;", (movie['id'], movie['original_language'], movie['title'], movie['overview'], release_date, movie['poster_path'], movie['vote_average'], id))
     conn.commit()
     conn.close()
     cursor.close()
@@ -92,7 +92,7 @@ def getReviews():
     cursor = conn.cursor()
 
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS reviews (review_id varchar PRIMARY KEY, author varchar, content varchar, movie_id int REFERENCES movies(movie_id));")
+        "CREATE TABLE IF NOT EXISTS Review (id varchar PRIMARY KEY, author varchar, content varchar, movie_id int REFERENCES Movie(id));")
     for id in movies:
         for count in tqdm(range(1, 4)):
             response = requests.get(
@@ -101,7 +101,7 @@ def getReviews():
             if len(response) > 0:
                 for i in tqdm(range(len(response))):
                     review = response[i]
-                    cursor.execute("INSERT INTO reviews (review_id, author, content, movie_id) VALUES (%s, %s, %s, %s) ON CONFLICT (review_id) DO NOTHING;",
+                    cursor.execute("INSERT INTO Review (id, author, content, movie_id) VALUES (%s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;",
                                    (review['id'], review['author'], review['content'], id))
     conn.commit()
     conn.close()
@@ -121,9 +121,9 @@ def getActors():
                       0: "Not specified", 3: "Non-binary"}
     cursor = conn.cursor()
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS actors (actor_id int PRIMARY KEY, name varchar, gender varchar);")
+        "CREATE TABLE IF NOT EXISTS Actor (id int PRIMARY KEY, name varchar, gender varchar);")
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS acts_in (actor_id int REFERENCES actors(actor_id), movie_id int REFERENCES movies(movie_id), PRIMARY KEY(actor_id, movie_id));")
+        "CREATE TABLE IF NOT EXISTS Acts_In (actor_id int REFERENCES Actor(id), movie_id int REFERENCES Movie(id), PRIMARY KEY(actor_id, movie_id));")
     for id in movies:
         response = requests.get(
             f'https://api.themoviedb.org/3/movie/{id}/credits?api_key={apiKey}&language=en-US')
@@ -133,9 +133,9 @@ def getActors():
             actor_id = cast['id']
             gender = gender_mapping[cast['gender']]
             movie_id = id
-            cursor.execute("INSERT INTO actors (actor_id, name, gender) VALUES (%s, %s, %s) ON CONFLICT (actor_id) DO NOTHING;",
+            cursor.execute("INSERT INTO Actor (id, name, gender) VALUES (%s, %s, %s) ON CONFLICT (id) DO NOTHING;",
                            (actor_id, name, gender))
-            cursor.execute("INSERT INTO acts_in (actor_id, movie_id) VALUES (%s, %s) ON CONFLICT (actor_id, movie_id) DO NOTHING;",
+            cursor.execute("INSERT INTO Acts_In (actor_id, movie_id) VALUES (%s, %s) ON CONFLICT (actor_id, movie_id) DO NOTHING;",
                            (actor_id, movie_id))
     conn.commit()
     conn.close()
